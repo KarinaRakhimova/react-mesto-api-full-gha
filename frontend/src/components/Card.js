9c1d344381e6ee0e47f2c60b789cb8394897e4f3
KarinaRakhimova <rahimova@list.ru>
2023-05-16 11:00:14 +0500

    fx isOwn bug2

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
-function Card({card, onCardClick, onCardLike, onCardDelete, isOwn}) {
+function Card({card, onCardClick, onCardLike, onCardDelete}) {
 
   const currentUser = React.useContext(CurrentUserContext)
   const [isLiked, setIsLiked] = React.useState(card.likes.some(i => i._id === currentUser._id))
+  const isOwn = card.owner._id === currentUser._id;
   const cardLikeButtonClassName = (
     `element__like-button ${isLiked && 'element__like-button_active'}`
   );
@@ -19,20 +20,35 @@ function Card({card, onCardClick, onCardLike, onCardDelete, isOwn}) {
   }
 
   return (
-    <li className="element" >
-      <img className="element__image" src={card.link} alt={card.name} onClick={() => handleCardClick(card)} />
+    <li className="element">
+      {isOwn && (
+        <button
+          className="element__delete"
+          aria-label="удалить"
+          type="button"
+          onClick={() => onCardDelete(card)}
+        ></button>
+      )}
+      <img
+        className="element__image"
+        src={card.link}
+        alt={card.name}
+        onClick={() => handleCardClick(card)}
+      />
       <div className="element__description">
         <h2 className="element__heading">{card.name}</h2>
         <div className="element__like">
-          <button className={cardLikeButtonClassName} aria-label="лайк" type="button" onClick={() => handleCardLike(card)}></button>
+          <button
+            className={cardLikeButtonClassName}
+            aria-label="лайк"
+            type="button"
+            onClick={() => handleCardLike(card)}
+          ></button>
           <p className="element__like-counter">{card.likes.length}</p>
         </div>
       </div>
-      {console.log(isOwn)}
-      {isOwn && <button className="element__delete" aria-label="удалить" type="button"
-        onClick={() => onCardDelete(card)}></button>}
     </li>
-  )
+  );
 }
 
 export default Card;
